258337d40271cddc892bc68bc132d2333fd506fc
devaio <robert.camp@outlook.com>
2016-05-11 11:17:20 -0600

    Add project files.

---
+++ OverwatchApp/HeroData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Data.Json;
+using Newtonsoft.Json;
+
+namespace OverwatchApp
+{
+    
+    public class Hero
+    {
+        public string HeroName { get; set; }
+        //ublic string CompositionName { get; set; }
+        //ublic DateTime ReleaseDateTime { get; set; }
+    }
+    public class HeroManager
+    {
+        public static JsonValue HeroData;
+        public static List<Hero> GetHeroes()
+        {
+            
+            var heroes = new List<Hero>();
+
+            HeroData = JsonValue.Parse(File.ReadAllText(@"./heroData.json"));
+            //HeroData.GetArray().ToList;
+
+
+            heroes.Add(new Hero
+            {
+                HeroName = "Winston",
+
+            });
+            heroes.Add(new Hero
+            {
+                HeroName = "Reinhardt",
+
+            });
+            heroes.Add(new Hero
+            {
+                HeroName = "Mei",
+
+            });
+
+            return heroes;
+        }
+
+
+    }
+
+        //public class HeroViewModel
+        //{
+        //    private Hero defaultHero = new Hero();
+        //    public Hero DefaultHero { get { return this.defaultHero; } }
+        //}
+    
+}
---
+++ OverwatchApp/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Data.Json;
+using Newtonsoft.Json;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OverwatchApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+
+    public sealed partial class MainPage : Page
+    {
+        double DeviceWidth;
+        double DeviceHeight;
+        
+        public Hero SelectedHero;
+        private List<Hero> heroes;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            heroes = HeroManager.GetHeroes();
+            System.Diagnostics.Debug.Write(heroes);
+            DeviceWidth = Window.Current.Bounds.Width;
+            DeviceHeight = Window.Current.Bounds.Height;
+            
+            //System.Diagnostics.Debug.Write(DeviceWidth);
+
+            // Grabbing File
+            //HeroData = JsonValue.Parse(File.ReadAllText(@"./heroData.json"));
+            //System.Diagnostics.Debug.Write(HeroData);
+            
+        }
+
+        private void nameInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainNav.IsPaneOpen = !MainNav.IsPaneOpen;
+        }
+
+        private void MainNav_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Menu should be 25% of page width
+            double width = (DeviceWidth * 0.25) < 150 ? 150 : (DeviceWidth * 0.25);
+            MainNav.OpenPaneLength = width;
+
+            
+             
+            
+
+
+        }
+
+        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+            //System.Diagnostics.Debug.Write(  );
+            //SelectedHero = (Hero)e.ClickedItem;
+            //System.Diagnostics.Debug.Write(SelectedHero.HeroName);
+        }
+    }
+}
